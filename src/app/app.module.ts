85bbb680285cc249296f8b1e6eb5524a55fbdcca
patricia.oshiro@totvs.com.br <patricia.oshiro@totvs.com.br>
2018-02-04 20:44:49 -0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
+import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CriarCursoModule,
+    CursosModule
   ],
-  providers: [],
+  providers: [LogService], // esta com o escopo global
   bootstrap: [AppComponent]
 })
 export class AppModule { }
